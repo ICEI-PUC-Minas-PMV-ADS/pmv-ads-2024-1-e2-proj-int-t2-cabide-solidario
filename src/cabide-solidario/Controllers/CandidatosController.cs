2d2cabe8189b9ecb2bdd9a1ea4d28213faa3331d
estevaocota <estevaocota89@gmail.com>
2024-05-07 06:52:13 -0300

    Criação da View da tabela Candidatos

---
+++ src/cabide-solidario/Controllers/CandidatosController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using cabide_solidario.Models;
+
+namespace cabide_solidario.Controllers
+{
+    public class CandidatosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CandidatosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Candidatos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Candidatos.ToListAsync());
+        }
+
+        // GET: Candidatos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var candidato = await _context.Candidatos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (candidato == null)
+            {
+                return NotFound();
+            }
+
+            return View(candidato);
+        }
+
+        // GET: Candidatos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Candidatos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Email,Disponibilidade,Texto_Motivacao")] Candidato candidato)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(candidato);
+                await _context.SaveChangesAsync();
+                /*return RedirectToAction(nameof(Index));*/
+                return RedirectToAction("Index", "Home");
+            }
+            /*return View(candidato);*/
+            return View();
+        }
+
+        // GET: Candidatos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var candidato = await _context.Candidatos.FindAsync(id);
+            if (candidato == null)
+            {
+                return NotFound();
+            }
+            return View(candidato);
+        }
+
+        // POST: Candidatos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Email,Disponibilidade,Texto_Motivacao")] Candidato candidato)
+        {
+            if (id != candidato.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(candidato);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CandidatoExists(candidato.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(candidato);
+        }
+
+        // GET: Candidatos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var candidato = await _context.Candidatos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (candidato == null)
+            {
+                return NotFound();
+            }
+
+            return View(candidato);
+        }
+
+        // POST: Candidatos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var candidato = await _context.Candidatos.FindAsync(id);
+            if (candidato != null)
+            {
+                _context.Candidatos.Remove(candidato);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CandidatoExists(int id)
+        {
+            return _context.Candidatos.Any(e => e.Id == id);
+        }
+    }
+}
